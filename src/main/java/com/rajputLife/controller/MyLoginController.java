8d4cce32716f961affd2bdaaee743ca186bc3717
FirstCommit <Manjul@192.168.1.105>
2022-09-17 10:35:13 -0500

    refactor

---
+++ src/main/java/com/rajputLife/controller/MyLoginController.java
@@ -0,0 +1,49 @@
+package com.rajputLife.controller;
+
+import com.rajputLife.model.Product;
+import com.rajputLife.persistence.ProductRepository;
+import org.ocpsoft.rewrite.annotation.Join;
+import org.ocpsoft.rewrite.annotation.RequestAction;
+import org.ocpsoft.rewrite.el.ELBeanName;
+import org.ocpsoft.rewrite.faces.annotation.Deferred;
+import org.ocpsoft.rewrite.faces.annotation.IgnorePostback;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+@Controller(value = "myloginController")
+@Scope(value = "session")
+@Component(value = "myloginController")
+@ELBeanName(value = "myloginController")
+@Join(path = "/login", to = "/registered/product-list.jsf")
+public class MyLoginController {
+
+ 	private ProductRepository productRepository;
+
+	private List<Product> products;
+
+	@Autowired
+	public MyLoginController(ProductRepository productRepository){
+		this.productRepository = productRepository;
+	}
+
+	@Deferred
+	@RequestAction
+	@IgnorePostback
+	public void loadData() {
+		products = productRepository.findAll();
+	}
+
+	public List<Product> getProducts() {
+		return products;
+	}
+
+	public String delete(Product product) {
+		productRepository.delete(product);
+		loadData();
+		return null;
+	}
+}
